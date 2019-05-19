b141ecfb108212025af6e10af2fc16e5af57c3ac
Guli Kholmatova <gulikholmatova@gmail.com>
2019-05-19 15:39:08 -0400

    Updates

---
+++ precache-manifest.3cb31ebe14bd14d27d82f9a325f61d87.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3798306ebf77c58181316b1c97640824",
+    "url": "/inno-rotten-tomatoes/index.html"
+  },
+  {
+    "revision": "8e2b5f13357fead5453f",
+    "url": "/inno-rotten-tomatoes/static/css/2.b5f9436f.chunk.css"
+  },
+  {
+    "revision": "3335aa1b07444ef0e84a",
+    "url": "/inno-rotten-tomatoes/static/css/main.c12c7dff.chunk.css"
+  },
+  {
+    "revision": "8e2b5f13357fead5453f",
+    "url": "/inno-rotten-tomatoes/static/js/2.b915b813.chunk.js"
+  },
+  {
+    "revision": "3335aa1b07444ef0e84a",
+    "url": "/inno-rotten-tomatoes/static/js/main.bb763287.chunk.js"
+  },
+  {
+    "revision": "9ed404c52b96e52884aa",
+    "url": "/inno-rotten-tomatoes/static/js/runtime~main.29faa1a1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/inno-rotten-tomatoes/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/inno-rotten-tomatoes/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/inno-rotten-tomatoes/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/inno-rotten-tomatoes/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/inno-rotten-tomatoes/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
